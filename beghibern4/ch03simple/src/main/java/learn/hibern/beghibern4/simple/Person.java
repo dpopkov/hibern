461acb17e7d004f4ebec65c2eab102172198149d
Denis Popkov <pkvdenis@gmail.com>
2020-09-28 22:24:51 +0300

    hibern: beghibern4: ch03simple: add module and simple model for app

---
+++ beghibern4/ch03simple/src/main/java/learn/hibern/beghibern4/simple/Person.java
@@ -0,0 +1,23 @@
+package learn.hibern.beghibern4.simple;
+
+public class Person {
+    private String name;
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ beghibern4/ch03simple/src/main/java/learn/hibern/beghibern4/simple/Skill.java
@@ -0,0 +1,23 @@
+package learn.hibern.beghibern4.simple;
+
+public class Skill {
+    private String name;
+
+    public Skill() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Skill{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
